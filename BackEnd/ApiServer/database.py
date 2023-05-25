18670c9dc10c3fc88d5e02614e89f889022c00f5
XmasApple <y4blokof@yandex.ru>
2023-05-25 14:34:48 +0300

    Docker

--- BackEnd/ApiServer/database.py
+++ BackEnd/ApiServer/database.py
@@ -1,3 +1,5 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
@@ -5,7 +7,11 @@ from pydantic import PostgresDsn
 
 POSTGRES_USER = 'postgres'
 POSTGRES_PASSWORD = '1111'
-POSTGRES_SERVER = '127.0.0.1'
+print(os.environ.values())
+if os.environ.get('POSTGRES_SERVER'):
+    POSTGRES_SERVER = os.environ.get('POSTGRES_SERVER')
+else:
+    POSTGRES_SERVER = '127.0.0.1'
 POSTGRES_DB = 'leader_hack'
 
 SQLALCHEMY_DATABASE_URL = PostgresDsn.build(
